831179917956cc2ed44b359222779e140930860c
Emirhan Baysal <emirhanbaysal35@hotmail.com>
2024-06-06 12:12:45 +0300

    blog_start

--- routers/blog.py
+++ routers/blog.py
@@ -71,7 +71,6 @@ def get_blogs(blog_id: int, check_staff: Staff = Depends(token_check)):
 def get_blogs(category_id: int, check_staff: Staff = Depends(token_check)):
     staff_check, connection, cursor = check_staff
     if not staff_check: return JSONResponse(status_code=401, content={"status": False, "message": "staff_not_found"})
-    # sql = f'''select * from blog where content ->>'category_id' = '{blog_id}'   ; '''
     blogs = fetchall__dict2dot(cursor, f'''
     select 
         b.content ->> 'title' as blog_title,
@@ -93,6 +92,50 @@ def get_blogs(category_id: int, check_staff: Staff = Depends(token_check)):
         return blogs
     connection_close(connection, cursor)
     return {"status": "blog_not_found"}
+
+@router.get("/list",summary="slug a göre listeleme")
+def list_slug(category_slug:str,check_staff:Staff=Depends(token_check)):
+    staff_check , connection,curosr = check_staff
+    if not staff_check: return JSONResponse(status_code=401,content={'Status':False,"message":"staff_not_found"})
+
+    check_record = fetchall__dict2dot(curosr,f'''
+       select b.content ->> 'title' as blog_title,
+              b.content ->> 'contents' as blog_content,
+              b.content ->> 'created_date' as blog_created_date,
+              c.content ->> 'name' as category_name,
+              s.content ->> 'name' as author_name,
+              s.content ->> 'surname' as author_surname,
+              s.content ->> 'email' as author_email
+       from blog b
+       inner join 
+       category c on (b.content ->>'category_id')::int = c.id
+       
+       inner join 
+       staffs s on (b.content->> 'staff_id')::int = s.id
+       
+       where c.content ->> 'slug' like '{category_slug}%'
+       ;''')
+
+    if check_record:
+        return check_record
+    connection_close(connection,curosr)
+    return {"status":"blog_not_found"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @router.put("/update", summary="post staff")
